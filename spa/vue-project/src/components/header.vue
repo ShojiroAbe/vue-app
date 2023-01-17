2c4b7daa48817db62995fefe0c287c011900136c
ShojiroAbe <astook89@gmail.com>
2023-01-17 22:13:15 +0900

    Add Header設置（実装途中）

---
+++ spa/vue-project/src/components/header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <h1>Header</h1>
+    
+    <nav>
+      <ul>
+        <li>検索(仮)</li> 
+        <li>Log In</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+header {
+    width: 100%;
+    padding: 1rem 3rem;
+    box-sizing: border-box;
+    justify-content: space-between;
+    background-color: aquamarine;
+    display: flex;
+  }
+
+  nav {
+    width: 30%;
+    display: flex;
+    align-items: center;
+  }
+
+  ul {
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+</style>
